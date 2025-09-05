329647ec28e19e13dae02115e31ca4803971a6fd
Mahantesh B <manteshbalaganur439@gmail.com>
2025-09-06 02:12:41 +0530

    Implemented Ejs middleware,http headers, status code

--- hello.js
+++
@@ -1,3 +0,0 @@
-const math=require("./math");
-console.log("the addtion of 2 value is :", math.add(7,2));
-console.log("the multiplaction of 2 value is :", math.mul(7,2));
--- project/index.js
+++ project/index.js
@@ -1,10 +1,24 @@
 const express=require("express");
 const fs=require("fs"); // to file append
-const users=require("./MOCK_DATA.json");// data import 
+let users=require("./MOCK_DATA.json");// data import 
 const app=express();
 let PORT=8000;
 
+// middleware - plugins 
+app.use(express.urlencoded({extended:false}));
+
+app.use((req,res,next)=>{
+  console.log("hello from middleware 1");
+  next();
+});
+app.use((req,res,next)=>{
+  console.log("hello from middleware 2");
+  // return res.end("No data fetched ");
+  next();
+});
+
 app.get("/api/users",(req,res)=>{
+  res.setHeader("x-myname","mahantesh");
     return res.json(users);
 })
 // html render hoga 
@@ -16,27 +30,55 @@ app.get("/users",(req,res)=>{
     `;
     res.send(html);
 })
- // middleware - plugins 
-app.use(express.urlencoded({extended:false}));
+
 
 app.post("/api/users",(req,res)=>{
     //create users
     const body=req.body;
     users.push({ ...body,id:users.length+1});
     fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
-        return res.json({status:"success",id:users.length});
+        return res.status(201).json({status:"success",id:users.length});
     })
 })
 
-// app.patch("/api/users/:id",(req,res)=>{
-//     return res.json({status:"pending"});
-// })
-// // dynamically select kar sakte hai data koo 
-// app.get("/api/users/:id",(req,res)=>{
-//     const id=Number(req.params.id);
-//     const user=users.find((user)=>user.id ===id);
-//     return res.json(user);
-// })
+app.patch("/api/users/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const updates = req.body; // e.g. { age: 21 }
+  
+  let user = users.find(u => u.id === id);
+  if (!user) return res.status(404).send("User not found");
+
+  Object.assign(user, updates);  // update only given fields
+  res.json(user);
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  users = users.filter(u => u.id !== id);
+  res.send("User deleted");
+});
+
+app.delete("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const index = users.findIndex(u => u.id === id);
+  if (index === -1) return res.status(404).send("User not found");
+
+  users.splice(index, 1); // remove from array
+
+  // overwrite JSON file with updated users
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users, null, 2), (err) => {
+    if (err) return res.status(500).send("Error writing file");
+    res.send("User deleted successfully ");
+  });
+});
+
+// dynamically select kar sakte hai data koo 
+app.get("/api/users/:id",(req,res)=>{
+    const id=Number(req.params.id);
+    const user=users.find((user)=>user.id ===id);
+    return res.json(user);
+})
 
 app.listen(PORT,()=>{
     console.log(`server has been started at local port ${PORT} `);
